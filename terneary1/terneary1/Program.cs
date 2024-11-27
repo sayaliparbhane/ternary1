d811cba92c0d41a17ce7ada53195766d5ffc4204
sayaliparbhane <sauparbhane25@gmail.com>
2024-11-27 11:32:46 +0530

    Add files via upload

---
+++ terneary1/terneary1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace terneary1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i == 4)
+                {
+                    continue;
+                }
+               else if (i == 8)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+            }
+        }
+    }
+
